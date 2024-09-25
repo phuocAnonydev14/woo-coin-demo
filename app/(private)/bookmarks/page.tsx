85a538ecf5e4b1dd34eded1c0fe88444cf318060
phuoc <phuoc.anonydev2k3@gmail.com>
2024-09-25 15:14:58 +0700

    feat: deploy

--- app/(private)/bookmarks/page.tsx
+++ app/(private)/bookmarks/page.tsx
@@ -1,16 +1,13 @@
-import { PostDefault, PostSocial } from '@/components/post';
-import { mockPosts } from '@/mocks/post';
-
 export default function FeedPage() {
   return (
     <div className="flex flex-col gap-6">
-      {mockPosts.map((post) =>
-        post.type === 'social' ? (
-          <PostSocial key={post.content} post={post} />
-        ) : (
-          <PostDefault key={post.content} post={post} />
-        ),
-      )}
+      {/*{mockPosts.map((post) =>*/}
+      {/*  post.type === 'social' ? (*/}
+      {/*    <PostSocial key={post.content} post={post} />*/}
+      {/*  ) : (*/}
+      {/*    <PostDefault key={post.content} post={post} />*/}
+      {/*  ),*/}
+      {/*)}*/}
     </div>
   );
 }
